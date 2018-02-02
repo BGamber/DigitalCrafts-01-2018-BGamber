2912a8f7343b273f35a23249f42aa677b335fd09
Ben Gamber <ben@stickfishstudio.com>
2018-02-02 09:28:34 -0500

    Additional rename commit

--- 2-1/dedup.py
+++
@@ -1,8 +0,0 @@
-groceryList = ["spam", "eggs", "spam", "milk", "spam", "cheese", "spam"]
-dedupList = []
-
-for item in groceryList:
-    if item not in dedupList:
-        dedupList.append(item)
-
-print dedupList
