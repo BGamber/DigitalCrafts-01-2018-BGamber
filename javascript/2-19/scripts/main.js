0edf29bf12cdcf5663bb8d6e24967c5f6eb1a225
Ben Gamber <ben@stickfishstudio.com>
2018-02-20 15:43:19 -0500

    Day 2 of Javascript; 102 exercises!

--- javascript/2-19/scripts/main.js
+++ javascript/2-19/scripts/main.js
@@ -78,25 +78,153 @@ var factor = function (number) {
 
 var cipher = function (message, key) {
     var shift = 13;
-    var alphabet = 'abcdefghijklmnopqrstuvwxyz';
-    var alphaDict = {};
-    var numDict = {};
-    var messageArray = message.toLowerCase().split('');
-    var letterToNumArray = [];
-    var ciperArray = [];
-    var numToLetterArray = [];
-    for (i=0;i<alphabet.length;i++) {
-        alphaDict[alphabet[i]] = i + 1;
-        numDict[String(i+1)] = alphabet[i];
-    }
-    for (i=0;i<message.length;i++) {
-        letterToNumArray.push(alphaDict[messageArray[i]]);
-    }
+    var uniArray = [];
+    var cipherArray = [];
+    var newMessage;
     if (key !== undefined) {
         shift = key;
     }
-    for (i=0;i<message.length;i++) {
-        var newNum = letterToNumArray[i] + shift;
-        // TODO: Handle new numbers shifted
+    for (var i=0; i < message.length; i++) {
+        var newCode = message.charCodeAt(i) - shift;
+        if (message.charCodeAt(i) !== 32) {
+            uniArray.push(newCode);
+        } else {
+            uniArray.push(32);
+        }
     }
+    for (var i=0; i < uniArray.length; i++) {
+        cipherArray.push(String.fromCharCode(uniArray[i]));
+    }
+    newMessage = cipherArray.join('');
+    return newMessage;
+}
+
+var leetspeak = function (message) {
+    var messageUpper = message.toUpperCase();
+    var leetMessage = '';
+    var convertObj = {
+        A: '4',
+        E: '3',
+        G: '6',
+        L: '1',
+        O: '0',
+        S: '5',
+        T: '7'
+    }
+    for (var i=0; i < message.length; i++) {
+        if (messageUpper.charAt(i) in convertObj) {
+            leetMessage += String(convertObj[messageUpper.charAt(i)]);
+        } else {
+            leetMessage += messageUpper.charAt(i);
+        }
+    }
+    return leetMessage;
+}
+
+var longLongVowels = function (word) {
+    var newWord = '';
+    var vowelArray = ['a', 'e', 'i', 'o', 'u'];
+    for (var i=0; i < word.length; i++) {
+        newWord += word.charAt(i);
+        if (word.charAt(i) === word.charAt(i-1) && vowelArray.indexOf(word.charAt(i)) !== -1) {
+            newWord += word.charAt(i).repeat(3);
+        }
+    }
+    return newWord;
+}
+
+var sumNumbers = function (list) {
+    var total = 0;
+    for (var i=0; i < list.length; i++) {
+        total += list[i];
+    }
+    return total;
+}
+
+var positiveNumbers = function (list) {
+    var posArray = [];
+    for (var i=0; i < list.length; i++) {
+        if (list[i] >= 0) {
+            posArray.push(list[i]);
+        }
+    }
+    return posArray;
+}
+
+var matrixAdd = function (matrix1, matrix2) {
+    var newMatrix = [];
+    for (var i=0; i < matrix1.length; i++) {
+        newMatrix.push([]);
+        for (var j=0; j < matrix2.length; j++) {
+            newMatrix[i][j] = matrix1[i][j] + matrix2[i][j];
+        }
+    }
+    return newMatrix;
+}
+
+var matrixMultiply = function (matrix1, matrix2) {
+    var newMatrix = [];
+    for (var check=0; check < matrix1.length; check++) {
+        if (matrix1[check].length !== matrix2.length) {
+            return 'ERROR: matrices cannot be multiplied.';
+        }  
+    }
+    for (var i=0; i < matrix1.length; i++) {
+        newMatrix.push([]);
+    }
+    for (var col=0; col < matrix2[0].length; col++) {
+        for (var row=0; row < matrix1.length; row++) {
+            var pairResults = [];
+            for (var pair=0; pair < matrix1[0].length; pair++) {
+                pairResults.push(matrix1[row][pair] * matrix2[pair][col]);
+            }
+            newMatrix[row][col] = pairResults[0] + pairResults[1];
+        }
+    }
+    return newMatrix;
+}
+
+var rockPaperScissors = function (p1, p2) {
+    shapes = { rock: 1, paper: 2, scissors: 0 }
+    result = shapes[p1.play()] - shapes[p2.play()];
+    if (Math.abs(result) > 1) {
+        result *= -0.5;
+    }
+    if (result > 0) {
+        return `player 1 wins!`;
+    } else if (result < 0) {
+        return `player 2 wins!`;
+    } else {
+        return `player 1 and player 2 tied.`;
+    }
+}
+
+var vsRockPaperScissors = function (numPlayers) {
+    var playerlist = [];
+    for (var i = 0; i < numPlayers; i++) {
+        var player = {
+            name: ('player '+(i+1)),
+            play: function () {
+                var num = Math.floor(Math.random() * 3);
+                switch (num) {
+                    case 0:
+                        return 'rock';
+                    case 1:
+                        return 'paper';
+                    case 2:
+                        return 'scissors';
+                }
+            },
+            wins: 0
+        }
+        playerlist.push(player);
+    }
+    // while (playerlist.length > 0) {
+    var playerMoves = {};
+    for (var i=0; i < playerlist.length; i++) {
+        playerMoves[playerlist[i].name] = playerlist[i].play();
+    }
+    console.log(playerMoves);
+    // }
+    // return `${playerlist[0].name} wins!`;
 }
