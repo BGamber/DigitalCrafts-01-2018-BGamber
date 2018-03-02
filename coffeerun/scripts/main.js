92df6a64f63f8ce321c6cf4c588967e8a5a7a8d1
Ben Gamber <ben@stickfishstudio.com>
2018-03-02 09:30:20 -0500

    Code cleanup

--- coffeerun/scripts/main.js
+++ coffeerun/scripts/main.js
@@ -1,18 +1,9 @@
-// var orderList = [];
 var apiAddress = 'http://dc-coffeerun.herokuapp.com/api/coffeeorders/';
 var orderForm = document.querySelector('form');
 
 var orderTracker = function() {
     var orderList = [];
     return {
-        saveLocalOrderList: function() {
-            localStorage.setItem('orders', JSON.stringify(orderList));
-        },
-        loadLocalOrderList: function() {
-            if (localStorage.getItem('orders') !== null) {
-                orderList = JSON.parse(localStorage.getItem('orders'));
-            }
-        },
         loadOrderList: function() {
             console.log('Querying server...');
             var onlineList = [];
@@ -30,14 +21,9 @@ var orderTracker = function() {
             console.log('Query done.')
         },
         addOrder: function(order) {
-            // orderList.push(order);
-            // this.saveLocalOrderList();
             $.post(apiAddress, order, function() {
                 orderTracker.loadOrderList();
             });
-        },
-        removeOrder: function(index) {
-            orderList.splice(index, 1);
         },
         makeOrderString: function(order) {
             orderString = '';
@@ -49,7 +35,6 @@ var orderTracker = function() {
             return orderString;
         },
         updateOrderList: function() {
-            // this.loadLocalOrderList();
             var $pageList = $('#order-list');
             $pageList.empty();
             orderList.forEach(function(order, i) {
@@ -57,7 +42,6 @@ var orderTracker = function() {
                 let orderID = order['_id'].split('').splice(order['_id'].length - 2).join('');
                 let email = order['emailAddress'];
                 newListItem.setAttribute('data-id', orderID);
-                newListItem.setAttribute('data-email', order['emailAddress']);
                 let listText = (i+1)+'. '+orderTracker.makeOrderString(order);
                 newListItem.textContent = listText;
                 newListItem.classList.add('order-item')
@@ -97,46 +81,12 @@ var orderTracker = function() {
                 $button.click(function(event) {
                     event.preventDefault();
                 });
-                // $button.click(function(event) {
-                //     event.preventDefault();
-                //     let indx = $(this).parent()[0].getAttribute('data-id');
-                //     orderTracker.removeOrder(indx);
-                //     orderTracker.updateOrderList();
-                // });
                 newListItem.appendChild($button[0]);
                 $pageList.append(newListItem);
             });
-            // this.saveLocalOrderList();
         }
     };
 }();
-
-// var updateOrderList = function() {
-//     var $pageList = $('#order-list');
-//     $pageList.empty();
-//     orderList.forEach(function(order, i) {
-//         let newListItem = document.createElement('li');
-//         let listText = "";
-//         newListItem.setAttribute('data-id', i);
-//         for (key in order) {
-//             listText += (`${key}: ${order[key]} - `);
-//         };
-//         newListItem.textContent = listText;
-//         newListItem.classList.add('order-item')
-//         let $button = $('<a>').attr('href', '#').append($('<span>'));
-//         $button.addClass('complete-button');
-//         $button.text('Complete Order');
-//         $button.click(function(event) {
-//             event.preventDefault();
-//             let indx = $(this).parent()[0].getAttribute('data-id');
-//             orderList.splice(indx, 1);
-//             updateOrderList();
-//         });
-//         newListItem.appendChild($button[0]);
-//         $pageList.append(newListItem);
-//     });
-//     localStorage.setItem('orders', JSON.stringify(orderList));
-// };
 
 orderForm.addEventListener('submit', function(event) {
     event.preventDefault();
@@ -171,13 +121,5 @@ $pop.click(function(event) {
     }, 0);
 });
 
-// IDEA: Create object (via factory) for storing data and functions
-// - Add, remove, load, and save orders to storage; update page
-// - tracks order list without app direct access
-
-// Once everything else is set, load data if it exists!
-// if (localStorage.getItem('orders') !== null) {
-//     orderList = JSON.parse(localStorage.getItem('orders'));
-//     updateOrderList();
-// }
+// Once everything else is set, load server data!
 orderTracker.loadOrderList();
