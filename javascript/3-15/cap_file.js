b45baa99f0109f27ef6686f377675646e88a8e45
Ben Gamber <ben@stickfishstudio.com>
2018-08-14 16:13:04 -0400

    DIR Review, Update

--- javascript/3-15/cap_file.js
+++ javascript/3-15/cap_file.js
@@ -1,20 +1,19 @@
-const fs = require('fs');
-const readline = require('readline');
+const fs = require("fs");
+const readline = require("readline");
 
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout
 });
 
-rl.question('Enter file name: ', (answer) => {
+rl.question("Enter file name: ", answer => {
+  rl.close();
   fs.readFile(answer, (err, data) => {
     if (err) {
       console.log(err.toString());
     } else {
       console.log(data.toString().toUpperCase());
       looping = false;
-    };
+    }
   });
 });
-
-rl.close();
